74bbdb4563ca2af6846b030df21d93a1a1347f54
Pevin Kumar <bevinbalaji@gmail.com>
2025-03-02 07:46:07 +0000

    server working fine

--- server.py
+++ server.py
@@ -3,11 +3,11 @@ import socket
 
 class file_handler:
     def get_file_content(self, file):
-        pass
-
+        with open("src/"+file, 'r') as f:
+            return f.read()
 class socket_handler:
     def __init__(self):
-        self.host=''
+        self.host='172.30.16.116'
         self.port=8080
     def create_socket(self):
         sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,23 +16,58 @@ class socket_handler:
         return sock
 
 class http_handler:
-    def request(self):
-        pass
+    def __init__(self):
+        self.file_handler = file_handler()
+        
+    def get_requested_file(self, data):
+        requested_file = data.split('\n')[0].split(' ')[1]
+        if requested_file == '/':
+            return 'index.html'
+        if requested_file.startswith('src'):
+            pass
+        if requested_file == 'favicon.ico':
+            pass
+        return requested_file[1:] 
+    
+    def get_request_method(self,data):
+        return data.split('\n')[0].split(' ')[0]
+    
+    def get_connected_host(self, data):
+        return data.split('\n')[1].split(' ')[1]
+    
+    def response(self,file):
+        return self.file_handler.get_file_content(file)
 
 class webserver:
     def __init__(self):
         self.file_handler = file_handler()
         self.http_handler = http_handler()
         self.socket_handler = socket_handler()
+        
     def webserver_handler(self):
         sock=self.socket_handler.create_socket()
         sock.listen()
         print('Server started on port %s' % self.socket_handler.port)
-        conn, addr = sock.accept()
-        print('Connected by', addr)
-        data = conn.recv(1024)
-        print('Received', repr(data))
-
+        while True:
+            try:
+                conn, addr = sock.accept()
+                # conn.sendall(self.http_handler.response('index.html').encode())
+                data=conn.recv(2048).decode()
+                
+                
+                requested_file=self.http_handler.get_requested_file(data)
+                requested_method=self.http_handler.get_request_method(data)
+                requested_host=self.http_handler.get_connected_host(data)
+                
+                
+                if requested_file != 'favicon.ico':
+                    print(f"Connected host: {requested_host} Requested file: {requested_file} Requested method :{requested_method}")
+                    conn.sendall(self.http_handler.response(requested_file).encode())
+                conn.close()
+            except KeyboardInterrupt:
+                print('\nServer shutting down...')
+                sock.close()
+                break
 
 def main():
     server = webserver()
