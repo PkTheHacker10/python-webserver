b59c44b4be905b9066aa4dae6adc0b4b12a666f7
pk <pk_server@pkserver@quickcom.com>
2025-03-06 08:05:21 +0000

    multiple class instence creation bug fixed

--- server/webserver.py
+++ server/webserver.py
@@ -10,26 +10,22 @@ except ImportError as Ie:
 class webserver:
     def __init__(self):
         # Gathering all classes from modules.
-        self.socket_handler_class=socket_handler()
-        self.http_handler_class=http_handler()
-        self.file_handler_class=file_handler()
+        self.socket_handler=socket_handler()()
+        self.http_handler=http_handler()()
+        self.file_handler=file_handler()()
         
     def webserver_handler(self):
         # Web server handler funtion to handle the web server.
-        # Initializing all classes.
-        _socket_handler=self.socket_handler_class()
-        _http_handler=self.http_handler_class()
-        _file_handler=self.file_handler_class()
-        sock=_socket_handler.create_socket()
+        sock=self.socket_handler.create_socket()
         sock.listen()
         while True:
             try:
                 conn, addr = sock.accept()
                 request_data=conn.recv(2048).decode()
-                requested_doc=_http_handler.http_handler(request_data)
-                requested_file=_http_handler.get_requested_file(request_data)
-                requested_method=_http_handler.get_request_method(request_data)
-                requested_host=_http_handler.get_connected_host(request_data)       
+                requested_doc=self.http_handler.http_handler(request_data)
+                requested_file=self.http_handler.get_requested_file(request_data)
+                requested_method=self.http_handler.get_request_method(request_data)
+                requested_host=self.http_handler.get_connected_host(request_data)       
                 print(f"Connected host: {requested_host.split(":")[0]} Requested file: {requested_file} Requested method :{requested_method}")
                 conn.sendall(requested_doc.encode())
                 conn.close()
